d83fb7e063a963495a4dbf2edb6f7d1f25cf197b
Bert Wagner <github@bertwagner.com>
2019-03-15 19:17:10 -0400

    Adds white frame in between edits

--- gopro-hilight-reel/MovieCreator.py
+++ gopro-hilight-reel/MovieCreator.py
@@ -1,12 +1,16 @@
 from moviepy.editor import *
 
-def EditVideo(videos,output_path):
+def EditVideo(videos,output_path,output_height,output_width):
     trimmed_clips = []
+    #Color  clip to flash  in between cuts
+    color_clip =  ColorClip((output_height,output_width),[255,255,255],False,.5,None)
     for video in videos:
         for cut in video.cuts:
             vid = VideoFileClip(video.path).subclip(cut.start/1000.0,cut.stop/1000.0)
             trimmed_clips.append(vid)
-        
+            trimmed_clips.append(color_clip)
+
+    
 
     #print(GoProVideos)
     final_video = concatenate_videoclips(trimmed_clips)
--- gopro-hilight-reel/app.py
+++ gopro-hilight-reel/app.py
@@ -5,11 +5,14 @@ if __name__ == '__main__':
     video_folder_path = 'E:\Documents\GitHub\GoPro-HiLight-Reel\gopro-hilight-reel'
     seconds_pre = 5000
     seconds_post = 5000
+    output_width=1920
+    output_height=1080
     
 
     videos = HiLightExtractor.get_video_properties(video_folder_path)
+    #print(videos)
     videos = CutCalculator.CalculateCuts(videos, seconds_pre, seconds_post)
-    MovieCreator.EditVideo(videos,video_folder_path)
+    MovieCreator.EditVideo(videos,video_folder_path,output_height,output_width)
     #UploadToYouTube()
 
     
