3fb71bf2760b543bf482f5bedeb1fa304ef42793
Bert Wagner <github@bertwagner.com>
2019-03-20 11:12:57 -0400

    Changes clip to black

--- gopro-hilight-reel/MovieCreator.py
+++ gopro-hilight-reel/MovieCreator.py
@@ -3,7 +3,7 @@ from moviepy.editor import *
 def EditVideo(videos,output_path,output_height,output_width):
     trimmed_clips = []
     #Color  clip to flash  in between cuts
-    color_clip =  ColorClip((output_height,output_width),[255,255,255],False,.5,None)
+    color_clip =  ColorClip((output_height,output_width),[0,0,0],False,.3,None)
     for video in videos:
         for cut in video.cuts:
             vid = VideoFileClip(video.path).subclip(cut.start/1000.0,cut.stop/1000.0)
